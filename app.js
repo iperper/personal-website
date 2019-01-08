af1db2451905116a30b532602bece43c38c4de92
Isaac Perper <isaac.perper@gmail.com>
2019-01-07 21:23:04 -0500

    Made images smaller for loading and added script to prevent sleeping

--- app.js
+++ app.js
@@ -5,6 +5,12 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var stylus = require('stylus');
 
+//Keep website awake
+var http = require("http");
+setInterval(function() {
+    http.get("http://perper-website.herokuapp.com");
+}, 300000); // every 5 minutes (300000)
+
 var indexRouter = require('./routes/index');
 // var portfolioRouter = require('./routes/portfolio') Not working put it in Index for now
 var usersRouter = require('./routes/users');
