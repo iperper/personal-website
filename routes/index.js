c7aaab13668a8f25ea72e4c1f9b034a6d2716ce3
Isaac Perper <iperper@mit.edu>
2019-07-10 07:45:19 -0700

    Fixed typo in Portfolio header.  Fixed front page anchors.  Enables markdown support for projects and converted existing project files to markdown.

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var path = require('path');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -7,7 +8,19 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/temp_project', function(req, res, next) {
+	console.log(req.params);
   res.render('temp_project', {title: 'Isaac Perper'});
+});
+
+router.get("/projects/*", function(req, res,next) {
+	const pageUrl = req.params['0'];
+	if (pageUrl){
+		const projectUrl = path.join('projects', pageUrl);
+		res.render(projectUrl);
+	}
+	else{
+		res.render("temp_project", {title: 'Isaac Perper'});
+	}
 });
 
 module.exports = router;
--- routes/portfolio.js
+++ routes/portfolio.js
@@ -2,8 +2,22 @@ var express = require('express');
 var router = express.Router();
 
 /* GET Project page. */
-router.get('/temp_project', function(req, res, next) {
-  res.render('temp_project', {title: 'Isaac Perper'});
+// router.get('../temp_project', function(req, res, next) {
+//   res.render('temp_project', {title: 'Isaac Perper'});
+// });
+
+router.get('colorwave', function(req, res, next) {
+  res.render('colorwave', {title: 'Isaac Perper'});
 });
+// router.get("*", function(req, res) {
+// 	console.log(req);
+// 	// console.log("Res", res);
+// 	const pageUrl = req.params['0'];
+// 	// console.log("Page url");
+// 	console.log(req);
+// 	const projectUrl = path.join('projects', 'colorwave');
+// 	console.log(projectUrl);
+// 	res.render('/projects/colorwave');
+// });
 
 module.exports = router;
